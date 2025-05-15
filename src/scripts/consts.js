4c15ddacfeb114c4bf1374c39f530c7b9c5f9cb6
David Sousa <72691307+davidsousadev@users.noreply.github.com>
2025-05-15 10:32:44 -0300

    Update consts.js remoção de -

--- src/scripts/consts.js
+++ src/scripts/consts.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://api-buy-tech.onrender.com';
 export const API_URL_LOCAL = 'http://0.0.0.0:8000'; // http://localhost:8000
-export const FRONT_URL = 'https://buy-tech.vercel.app';
+export const FRONT_URL = 'https://buytech.vercel.app';
